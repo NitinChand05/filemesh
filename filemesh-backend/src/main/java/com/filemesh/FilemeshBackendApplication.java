883f4249bcbc1a6b1c0346c9b4795fb9da3b98ef
NitinChand05 <nitnchand@gmail.com>
2026-08-18 22:59:18 +0530

    backend

---
+++ filemesh-backend/src/main/java/com/filemesh/FilemeshBackendApplication.java
@@ -0,0 +1,13 @@
+package com.filemesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilemeshBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilemeshBackendApplication.class, args);
+	}
+
+}
---
+++ filemesh-backend/src/test/java/com/filemesh/FilemeshBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filemesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilemeshBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
